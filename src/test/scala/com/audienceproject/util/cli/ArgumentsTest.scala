5e56858d30c7bdcf0e9f360c70224897ad979519
Cosmin Catalin Sanda <cosmincatalin@gmail.com>
2018-10-29 10:43:30 +0100

    refactor the code and add a new method

--- src/test/scala/com/audienceproject/util/cli/ArgumentsTest.scala
+++ src/test/scala/com/audienceproject/util/cli/ArgumentsTest.scala
@@ -6,13 +6,13 @@ class ArgumentsTest extends FunSuite {
 
     test("When provided with an array of arguments that are in pairs where the first key starts with -- and the value follows" +
         " the instance should initialize correctly") {
-        implicit val sample = Array[String]("--asdfsaf", "asdfasdf", "--asdfasdfasdf", "asdfsadf", "--asdfasdf", "asdfasdfasdf")
+        implicit val sample: Array[String] = Array[String]("--asdfsaf", "asdfasdf", "--asdfasdfasdf", "asdfsadf", "--asdfasdf", "asdfasdfasdf")
         val arguments = new Arguments
         assert(arguments != null)
     }
 
     test("The instance should not initialize when the first argument does not start with --") {
-        implicit val sample = Array[String]("-asdfsaf", "asdfasdf", "--asdfasdfasdf", "asdfsadf", "--asdfasdf", "asdfasdfasdf")
+        implicit val sample: Array[String] = Array[String]("-asdfsaf", "asdfasdf", "--asdfasdfasdf", "asdfsadf", "--asdfasdf", "asdfasdfasdf")
         val thrown = intercept[Exception] {
             new Arguments
         }
@@ -21,20 +21,20 @@ class ArgumentsTest extends FunSuite {
 
     test("When provided with an array of two arguments where the first start with -- and the second is the value" +
         " calling getOption on the key should return the defined corresponding value") {
-        implicit val sample = Array[String]("--akey", "justavalue")
+        implicit val sample: Array[String] = Array[String]("--akey", "justavalue")
         val arguments = new Arguments
         assert(arguments.getOption("akey").get.equals("justavalue"))
     }
 
     test("When provided with an array of two arguments where the first start with -- and the second is a" +
         " coma separated list of values, calling getOptionArray on the key should return an array of values") {
-        implicit val sample = Array[String]("--akey", "value1,value2,value3")
+        implicit val sample: Array[String] = Array[String]("--akey", "value1,value2,value3")
         val arguments = new Arguments
         assert(arguments.getOptionArray("akey").get.length == 3)
     }
 
     test("The instance should initialize correctly when the last argument is a flag") {
-        implicit val sample = Array[String]("--asdfsaf", "asdfasdf", "--local")
+        implicit val sample: Array[String] = Array[String]("--asdfsaf", "asdfasdf", "--local")
         val arguments = new Arguments
         assert(arguments.isSet("local"))
     }
